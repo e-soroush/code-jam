41c7776d3642fb27370c5071a8c002b64bb871f9
e-soroush <ebrahim.soroush@gmail.com>
2018-04-24 23:29:37 +0430

    adds cpp templates

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include<set>
+#include<map>
+#include<list>
+#include<queue>
+#include<stack>
+#include<string>
+#include<math.h>
+#include<time.h>
+#include<vector>
+#include<bitset>
+#include<memory>
+#include<utility>
+#include<fstream>
+#include<stdio.h>
+#include<sstream>
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+#include<algorithm>
+#include <numeric>
+
+
+
+void solve() {
+
+}
+
+int main(int argc, char *argv[])
+{
+    int T;
+    std::cin >> T;
+    for(int i=1; i<=T; ++i) {
+        std::cout << "Case #" << i << ": ";
+        solve();
+    }
+    return 0;
+}
